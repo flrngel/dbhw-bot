91034e98c6ae5b6000c660e70f3392f7a6d7e329
kunwookim <drama0708@gmail.com>
2016-06-13 21:53:39 +0900

    I alter the Company; default value(nike shoes, adidas shoes, trash)

--- customer/Client/src/main/java/com/client/ClientApplication.java
+++ customer/Client/src/main/java/com/client/ClientApplication.java
@@ -24,8 +24,8 @@ public class ClientApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		toCompanyRepository.save(Arrays.asList(new ToCompany("nike shoes", 10000, "https://www.google.co.kr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiA-sa57qTNAhXIQpQKHX5pDYgQjRwIBw&url=http%3A%2F%2Fwww.footlocker.com%2FMens%2FNike%2FShoes%2F_-_%2FN-24ZzzZrj&psig=AFQjCNFR-C7bP_90By9vX6bcLxPRDxkAHQ&ust=1465902485704933", "http://www.auction.co.kr/", "uuid-1", true),
-				new ToCompany("adidas shoes", 10000, "https://www.google.co.kr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiA-sa57qTNAhXIQpQKHX5pDYgQjRwIBw&url=http%3A%2F%2Fwww.footlocker.com%2FMens%2FNike%2FShoes%2F_-_%2FN-24ZzzZrj&psig=AFQjCNFR-C7bP_90By9vX6bcLxPRDxkAHQ&ust=1465902485704933", "http://www.auction.co.kr/", "uuid-1", true),
-				new ToCompany("trash", 10000, "https://www.google.co.kr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjv_pOR76TNAhVGk5QKHeRwDBoQjRwIBA&url=http%3A%2F%2Fwww.cdrental.co.kr%2Fproduct%2Flist.html%3Fcate_no%3D114&psig=AFQjCNGb_foYFFjenewSXWigFO3cYcQJLg&ust=1465902671253678", "http://www.auction.co.kr/", "uuid-1", true)));
+		toCompanyRepository.save(Arrays.asList(new ToCompany("nike shoes", 10000, "https://www.google.co.kr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiA-sa57qTNAhXIQpQKHX5pDYgQjRwIBw&url=http%3A%2F%2Fwww.footlocker.com%2FMens%2FNike%2FShoes%2F_-_%2FN-24ZzzZrj&psig=AFQjCNFR-C7bP_90By9vX6bcLxPRDxkAHQ&ust=1465902485704933", "http://www.auction.co.kr/", "8091", true),
+				new ToCompany("adidas shoes", 10000, "https://www.google.co.kr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiA-sa57qTNAhXIQpQKHX5pDYgQjRwIBw&url=http%3A%2F%2Fwww.footlocker.com%2FMens%2FNike%2FShoes%2F_-_%2FN-24ZzzZrj&psig=AFQjCNFR-C7bP_90By9vX6bcLxPRDxkAHQ&ust=1465902485704933", "http://www.auction.co.kr/", "8091", true),
+				new ToCompany("trash", 10000, "https://www.google.co.kr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjv_pOR76TNAhVGk5QKHeRwDBoQjRwIBA&url=http%3A%2F%2Fwww.cdrental.co.kr%2Fproduct%2Flist.html%3Fcate_no%3D114&psig=AFQjCNGb_foYFFjenewSXWigFO3cYcQJLg&ust=1465902671253678", "http://www.auction.co.kr/", "8091", true)));
 	}
 }
